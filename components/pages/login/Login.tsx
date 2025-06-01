27f744c235519c2f4ddccce1915e89ee4d6f8369
Zahra <zahra16toa@gmail.com>
2025-06-01 23:17:51 +0330

    feat: create the login section and show the logo component

--- components/pages/login/Login.tsx
+++ components/pages/login/Login.tsx
@@ -1,11 +1,12 @@
 import Card from "@/components/globals/Card"
+import Logo from "@/components/globals/Logo"
 
 const Login = () => {
     return (
     <>
-        <Card>
-            {/* <Logo /> */}
-            <h3>ورود به پنل</h3>
+        <Card className="!w-1/4 flex flex-col items-center justify-center gap-10">
+            <Logo isShowTitle={false} />
+            <h3 className="font-bold">ورود به پنل</h3>
             {/* <LoginForm /> */}
         </Card>
     </>
