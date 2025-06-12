aeae7433b0bef16ec56d5940bbc03cfda647c997
Zahra <zahra16toa@gmail.com>
2025-06-12 16:29:58 +0330

    feat: creae card component to add elements on this

--- components/globals/Card.tsx
+++ components/globals/Card.tsx
@@ -7,7 +7,7 @@ const Card = ({
   children: ReactNode;
   className?: string;
 }) => {
-  return <section className={`bg-white p-10 rounded-2xl shadow ${className}`}>{children}</section>;
+  return <section className={`bg-white p-10 rounded-3xl shadow ${className}`}>{children}</section>;
 };
 
 export default Card;
