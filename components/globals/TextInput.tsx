22aeb342d8a2b3b8253595b5c6280fcc0e705bf5
Zahra <zahra16toa@gmail.com>
2025-06-12 16:31:13 +0330

    feat: create customize text input component

--- components/globals/TextInput.tsx
+++ components/globals/TextInput.tsx
@@ -1,4 +1,4 @@
-import { HTMLInputTypeAttribute } from "react";
+import { HTMLInputTypeAttribute, ReactNode } from "react";
 
 const TextInput = ({
   label,
@@ -6,25 +6,30 @@ const TextInput = ({
   type,
   className,
   placeholder,
-  dir
+  dir,
+  icon,
 }: {
   label?: string;
   id?: string;
   type: HTMLInputTypeAttribute;
   className?: string;
   placeholder?: string;
-  dir?: "ltr" | "rtl"
+  dir?: "ltr" | "rtl";
+  icon?: ReactNode;
 }) => {
   return (
-    <div className="flex flex-col items-start gap-2 w-full px-5">
+    <div className="flex flex-col items-start gap-2 w-full">
       <label htmlFor={id}>{label}</label>
-      <input
-        type={type}
-        dir={dir}
-        className={`p-2 rounded-xl bg-[#d9d9d95b] outline-0 px-3 focus:shadow-lg w-full shadow-sm ${className}`}
-        placeholder={placeholder}
-        id={id}
-      />
+      <div className="w-full relative">
+        {icon}
+        <input
+          type={type}
+          dir={dir}
+          className={`p-2 rounded-xl bg-[#d9d9d95b] outline-0 px-3 focus:shadow-lg w-full shadow-sm ${className} ${icon ? 'pl-12' : ''}`}
+          placeholder={placeholder}
+          id={id}
+        />
+      </div>
     </div>
   );
 };
