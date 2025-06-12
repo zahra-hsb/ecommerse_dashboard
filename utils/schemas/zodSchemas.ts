6929fcbddd1f7d57bb21183d3803ebe55fb7a5cd
Zahra <zahra16toa@gmail.com>
2025-06-12 19:15:22 +0330

    feat: enhance login schema to improve error handling for username and password

--- utils/schemas/zodSchemas.ts
+++ utils/schemas/zodSchemas.ts
@@ -1,10 +1,13 @@
+
 import { z } from "zod/v4";
 
 
 
 const LoginSchema = z.object({
-    username: z.string({ required_error: "نام کاربری الزامی است" }).regex(/^@.*$/i, "نام کاربری باید با @ شروع شود"),
-    password: z.string({ required_error: "رمز عبور الزامی است" }).min(8, "رمز عبور باید حداقل 8 کارکتر داشته باشد"),
+    username: z.string().nonempty("نام کاربری الزامی است").regex(/^@.*$/i, "نام کاربری باید با @ شروع شود"),
+    password: z.string().min(8, "رمز عبور باید حداقل 8 کارکتر داشته باشد").refine(value => value !== "", {
+        message: "رمز عبور الزامی است",
+    }),
 })
 
 
