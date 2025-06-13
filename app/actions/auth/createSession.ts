5e2225401c48a65911799370d294c002613d1958
Zahra <zahra16toa@gmail.com>
2025-06-13 18:53:26 +0330

    feat: add create session server action

---
+++ app/actions/auth/createSession.ts
@@ -0,0 +1,46 @@
+"use server";
+import { dbConnect } from "@/lib/db";
+import Session from "@/utils/models/Sessions";
+import { getDeviceInfo } from "../device/getDeviceInfo";
+import { sign } from "../jwt/jwt";
+import { cookies } from "next/headers";
+// import { encryptString } from "./encryptString";
+
+export async function createSession(payload: {
+  _id: string;
+  username: string;
+  email: string;
+}) {
+  try {
+    const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    const userAgent = await getDeviceInfo();
+    const jwt = await sign({
+      _id: payload._id,
+      username: payload.username,
+      email: payload.email,
+    });
+    // console.log('ghfdhgfhgf => ', await encryptString(jwt, 50))
+    await dbConnect();
+    const sessionData = await Session.insertOne({
+      user_id: payload._id,
+      expires_at: expiresAt,
+      session_time: Date.now(),
+      device_info: userAgent,
+      jwt,
+      //   jwt: await encryptString(jwt, 50)
+    });
+    console.log("session => ", sessionData);
+    (await cookies()).set({
+      name: "auth-token",
+      value: jwt, // encrypt
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 1000, // 1 day
+    });
+    
+  } catch (err) {
+    console.log("error while create session ", err);
+  }
+}
