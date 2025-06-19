cce9cb82b8c81b2e9cf7ff9331ac3bef05d831cc
Zahra <zahra16toa@gmail.com>
2025-06-19 12:25:56 +0330

    feat: create check athentication to redirect in pannel

--- components/pages/AuthProvider/index.tsx
+++ components/pages/AuthProvider/index.tsx
@@ -1,11 +1,22 @@
 "use client"
 // import { getSessionInfo } from "@/app/actions/auth/getSessionInfo";
 import { userStore } from "@/utils/stores/userStore";
-import { ReactNode } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { ReactNode, useEffect } from "react";
 
 const AuthProvider = ({ children }: { children: (Element | ReactNode)[] }) => {
   const { userInfo } = userStore();
+  const router = useRouter()
+  const pathname = usePathname()
   console.log(userInfo)
+
+  useEffect(() => {
+    if(!userInfo && pathname !== "/") {
+      router.push('/')
+    } else {
+      router.push('/dashboard')
+    }
+  }, [userInfo, pathname, router])
 //   getSessionInfo(userInfo._id);
   return <>{children}</>;
 };
