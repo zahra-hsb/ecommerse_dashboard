a66e9e1bd0932bdbf18e19af17a22753653bf164
zahra <zahra16toa@gmail.com>
2025-08-23 16:31:07 +0330

    fix: change the create session codes and check the session if the token does not expired then it does not have to save it

--- app/actions/auth/createSession.ts
+++ app/actions/auth/createSession.ts
@@ -5,6 +5,7 @@ import { getDeviceInfo } from "../device/getDeviceInfo";
 import { sign } from "../jwt/jwt";
 import { encryptString } from "./encryptString";
 import { saveStrCookie } from "./saveStrCookie";
+// import User from "@/utils/models/Users";
 
 export async function createSession(payload: {
   _id: string;
@@ -12,45 +13,75 @@ export async function createSession(payload: {
   email: string;
 }) {
   try {
-    const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
-    const userAgent = await getDeviceInfo();
-    const jwt = await sign({
-      _id: payload._id,
-      username: payload.username,
-      email: payload.email,
-    });
-
-    const encryptedJWT = await encryptString(jwt, "jwt");
-
-    await dbConnect();
-    await Session.insertOne({
-      user_id: payload._id,
-      expires_at: expiresAt,
-      session_time: Date.now(),
-      device_info: userAgent,
-      // jwt,
-      jwt: encryptedJWT,
-    });
-    await saveStrCookie({
-      name: "auth-token",
-      str: encryptedJWT ? encryptedJWT.toString() : jwt,
-      isHttpOnly: true,
-      isSecure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      path: "/",
-      maxAge: 60 * 60 * 24 * 1000,
-    })
-    return {
-      ok: true,
-      status: 200,
-      message: "شما با موفقیت وارد شدید",
-      data: {
-        id: payload._id,
+    const now = Date.now();
+    console.log("now => ", now);
+    const session = await Session.find({ user_id: payload._id });
+    const foundSession = session.find(
+      (s) => new Date(s.expires_at) > new Date(now)
+    );
+    console.log("found => ", foundSession);
+    if (!foundSession) {
+      const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      const userAgent = await getDeviceInfo();
+      const jwt = await sign({
+        _id: payload._id,
         username: payload.username,
         email: payload.email,
-        token: encryptedJWT ? encryptedJWT?.toString() : "",
-      },
-    };
+      });
+
+      const encryptedJWT = await encryptString(jwt, "jwt");
+
+      await dbConnect();
+      await Session.insertOne({
+        user_id: payload._id,
+        expires_at: expiresAt,
+        session_time: Date.now(),
+        device_info: userAgent,
+        // jwt,
+        jwt: encryptedJWT,
+      });
+      await saveStrCookie({
+        name: "auth-token",
+        str: encryptedJWT ? encryptedJWT.toString() : jwt,
+        isHttpOnly: true,
+        isSecure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        path: "/",
+        maxAge: 60 * 60 * 24 * 1000,
+      });
+      return {
+        ok: true,
+        status: 200,
+        message: "شما با موفقیت وارد شدید",
+        data: {
+          id: payload._id,
+          username: payload.username,
+          email: payload.email,
+          token: encryptedJWT ? encryptedJWT?.toString() : "",
+        },
+      };
+    } else {
+      await saveStrCookie({
+        name: "auth-token",
+        str: foundSession.jwt.toString(),
+        isHttpOnly: true,
+        isSecure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        path: "/",
+        maxAge: 60 * 60 * 24 * 1000,
+      });
+      return {
+        ok: true,
+        status: 200,
+        message: "شما با موفقیت وارد شدید",
+        data: {
+          id: payload._id,
+          username: payload.username,
+          email: payload.email,
+          token: foundSession.jwt?.toString(),
+        },
+      };
+    }
   } catch (err) {
     console.log("error while create session ", err);
     return {
--- app/actions/product/addProductServerAction.tsx
+++ app/actions/product/addProductServerAction.tsx
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 import { dbConnect } from "@/lib/db";
 import Product from "@/utils/models/Products";
 import { ProductType } from "@/utils/schemas/types";
@@ -6,32 +6,35 @@ import { ProductType } from "@/utils/schemas/types";
 export async function addProductServerAction(values: ProductType) {
   try {
     await dbConnect();
-    const foundProduct = await Product.findOne({ title: values.title })
+    const foundProduct = await Product.findOne({ title: values.title });
     if (foundProduct) {
-        return {
-            ok: false,
-            status: 400,
-            message: "محصولی که میخواهید درج کنید از قبل وجود دارد"
-        }
+      return {
+        ok: false,
+        status: 400,
+        message: "محصولی که میخواهید درج کنید از قبل وجود دارد",
+        data: null
+      };
     } else {
-        // console.log("pro => ", values);
-        const newProduct = await Product.insertOne({ ...values });
-        
-        // console.log("pro here => ", newProduct);
-        return {
-            ok: true,
-            status: 200,
-            message: "محصول شما با موفقیت افزوده شد"
-        }
+      // console.log("pro => ", values);
+      const newProduct = await Product.insertOne({ ...values });
+
+      // console.log("pro here => ", newProduct);
+      return {
+        ok: true,
+        status: 200,
+        message: "محصول شما با موفقیت افزوده شد",
+        data: newProduct,
+      };
     }
     // newProduct.save();
     // console.log("added => ", addedProductResponse)
   } catch (error) {
     console.log("Error while creating products => ", error);
     return {
-        ok: false,
-        status: 500,
-        message: "خطا در افزودن محصول"
-    }
+      ok: false,
+      status: 500,
+      message: "خطا در افزودن محصول",
+      data: null,
+    };
   }
 }
